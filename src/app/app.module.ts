55914003abf4556375e3ce0fc9c6f56943e34173
Maga <87536225+MagBreis@users.noreply.github.com>
2021-10-06 15:58:49 -0300

    coverso de madidas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { ConversorModule } from './conversor';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ConversorModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/conversor/utils/modal-conversao.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { ConversorService } from '../services';
+import { Conversao, Medida } from '../models';
+
+@Component({
+  selector: 'modal-conversao',
+  templateUrl: './modal-conversao.component.html',
+  styleUrls: ['./modal-conversao.component.css']
+})
+export class ModalConversaoComponent implements OnInit {
+
+  @Input() id: string;
+  @Input() conversao: Conversao = new Conversao();
+  @Output() onConfirm: EventEmitter<any> = new EventEmitter<any>();  
+
+
+  constructor(private conversorService: ConversorService) { }
+
+  ngOnInit(): void {
+  }
+
+  novaConsulta() {
+    this.onConfirm.emit();
+  }
+
+  get valorConvertido(): string {
+    return (this.conversao.valor * this.conversorService.converter(this.conversao)).toFixed(2);
+  }
+
+
+}
